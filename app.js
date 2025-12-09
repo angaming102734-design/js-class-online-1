f7672501f8745b7121a0904cb4b2bbfc6e044e82
ather ali <a.ngaming102734@gmail.com>
2025-12-09 19:50:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+// alert()
+//  if (SAT > avg || GPA > 2.5 || sport === "football") 
+//     {  alert("Welcome to Bubba State!");  }  
+//  else {  alert("Have you looked into appliance repair?");  }
+
+// var weight = 1000;
+// var time = 10;
+
+// if(weight > 700 && time < 12){
+//     document.write(`<h1> Yes its right </h1>`)
+// }else{
+//     document.write(`No wrong `)
+// // }
+// var weight = 1000;
+// var time = 10;
+
+// if(weight > 700 && time > 12){
+//     document.write(`<h1> Yes its right </h1>`)
+// }else{
+//     document.write(`No wrong `)
+// }
+
+// if(weight > 700  time > 12){
+//     document.write(`<h1> Yes its right </h1>`)
+// }else{
+//     document.write(`No wrong `)
+// }
+
+// if (age > 65 || age < 21 && res === "U.S.") {
+
+// }
+
+// Array
+
+//  var myData =[];
+//  var myName=''
+// document.write(Array.isArray(myData));
+
+// primitive data type
+
+// 1number
+// 2 string
+// 3null
+// 4ind
+
+
+
+// non primitive data types 
+// 1. Array
+// 2. odject
+
+// var str ='' ;
+// document.write(typeof str);
+
+// var boll
+
+var arr1 =[12 ,13, 15 ,20, "yellow" , 'Salani' , true, null]
+
+// document.write(arr1.length)
+
+// document.write(arr1[5]+'<br>');
+// document.write(arr1[7]);
+
+document.write('before',arr1 +'<br>')
+
+var index2 = 100;
+
+arr1[2] =index2
+
+document.write('after',arr1+'<br>')
